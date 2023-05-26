794fe9350f8ba4e494e49b076d41f79953510528
kef <kekekefir@gmail.com>
2023-05-26 15:18:29 +0300

    page_2

--- pages/page_2.py
+++ pages/page_2.py
@@ -13,12 +13,12 @@ import time
 from typing import Tuple
 
 from torchvision.models import resnet18, ResNet18_Weights
-device = 'cpu'
+device = 'cuda'
 
 model = resnet18(weights=ResNet18_Weights.DEFAULT)
 model.to(device)
 model.fc = nn.Linear(512, 1)
-model.load_state_dict(torch.load('kotosobaki.pt').cpu())
+model.load_state_dict(torch.load('kotosobaki.pt'))
 model.eval()
 resize = T.Resize((224, 224))
 img = resize(io.read_image('dog.jpeg')/255)
