584c79465a01f661073ac96432f527dabcb0c44c
kef <kekekefir@gmail.com>
2023-05-26 12:49:34 +0300

    fsdfs

--- pages/page_2.py
+++ pages/page_2.py
@@ -19,7 +19,7 @@ model = resnet18(weights=ResNet18_Weights.DEFAULT)
 model.fc = nn.Linear(512, 1)
 #model.to(device).load_state_dict(torch.load('resnet_cats_dogs.py'))
 resize = T.Resize((224, 224))
-img = resize(io.read_image('cat.jpg')/255)
+img = resize(io.read_image('dog-puppy.jpg')/255)
 
 
 st.markdown("# Котики и собачки 🎉")
