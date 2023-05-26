3eaf9dc726bbed605f6f5ff5927b59fb021e4f70
kef <kekekefir@gmail.com>
2023-05-26 15:14:41 +0300

    page_2

--- pages/page_2.py
+++ pages/page_2.py
@@ -13,7 +13,7 @@ import time
 from typing import Tuple
 
 from torchvision.models import resnet18, ResNet18_Weights
-device = 'cpu'
+device = 'cuda'
 
 model = resnet18(weights=ResNet18_Weights.DEFAULT)
 model.fc = nn.Linear(512, 1)
