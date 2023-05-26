96541da14dd2aff3ecfe1052915e914164a7f8aa
kef <kekekefir@gmail.com>
2023-05-26 12:14:40 +0300

    fsdfs

--- pages/page_2.py
+++ pages/page_2.py
@@ -17,7 +17,7 @@ device = 'cpu'
 
 model = resnet18(weights=ResNet18_Weights.DEFAULT)
 model.fc = nn.Linear(512, 1)
-model.load_state_dict(torch.load('resnet_cats_dogs.py'))
+model.load_state_dict(torch.load('resnet_cats_dogs.py'), map_location=torch.device('cpu'))
 resize = T.Resize((224, 224))
 #img = resize(io.read_image('cat.jpg')/255)
 
